4d501045e8644f6172865d372c47a799bb0585c4
win-runner <joshua.f@posteo.de>
2024-05-18 14:30:55 +0200

    feat: add azurecaf_name via terraform_data

--- terraform-{{cookiecutter.primary_provider}}-{{cookiecutter.module_name}}/naming.tf
+++ terraform-{{cookiecutter.primary_provider}}-{{cookiecutter.module_name}}/naming.tf
@@ -1,10 +1,32 @@
 #// this generates names for the azure resources using the following pattern:
 #// [prefix]-[cafprefix]-[name]-[postfix]-[random_chars]
-# resource "azurecaf_name" "main" {
-#   name          = lower(var.name)
-#   random_length = 8
-#   clean_input   = true
-#   resource_types = [
-#     "azurerm_resource_group",
-#   ]
-# }
+#// Current supported resource types:
+#// https://registry.terraform.io/providers/aztfmod/azurecaf/latest/docs/resources/azurecaf_name
+
+locals {
+  resource_types = toset([])
+}
+
+data "azurecaf_name" "main" {
+  for_each = local.resource_types
+
+  name          = var.name
+  random_length = 8
+  clean_input   = true
+  resource_type = each.value
+}
+
+resource "terraform_data" "name" {
+  input = var.name
+}
+
+resource "terraform_data" "azurecaf_name" {
+  for_each = local.resource_types
+
+  input = data.azurecaf_name.main[each.key].result
+
+  lifecycle {
+    ignore_changes       = [input]
+    replace_triggered_by = [terraform_data.name]
+  }
+}
